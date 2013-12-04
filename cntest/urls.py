00bdeeb0836e3e2ad5306122a6f9c2f5795b49af
weetao <weetao@yeah.net>
2013-12-04 15:46:27 +0800

    fix cntest urls.py

--- cntest/urls.py
+++ cntest/urls.py
@@ -19,7 +19,7 @@ urlpatterns = patterns(
     url(r'^pclevel/', include('NCRE.urls')),
     url(r'^$', 'NCRE.views.check'),
     url(r'^login/$', 'NCRE.views.login_view'),
-    url(r'^logou/$', 'NCRE.views.logout_view'),
+    url(r'^logout/$', 'NCRE.views.logout_view'),
     url(r'^manage/$', 'NCRE.views.manage_view'),
     url(r'^qrcode/$','qr_code.views.my_qr'),
     url(r'^mycode/$',TemplateView.as_view(template_name='qrcode/qrcode.html')),
