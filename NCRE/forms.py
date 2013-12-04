849b1cfa89cd3440f88104485fb6808bb674ac9b
weitao <hedeath@163.com>
2013-12-04 21:31:09 +0800

    fix bug

--- NCRE/forms.py
+++ NCRE/forms.py
@@ -6,6 +6,8 @@ from NCRE.models import NCRE
 class CheckForm(forms.Form):
     testId = forms.CharField(max_length=18, label=u'准考证号', required=True,
                              widget=forms.TextInput(attrs={'class': 'input-medium', 'required': 'required'}))
+    stuId = forms.CharField(max_length=4, label=u'身份证号后四位', required=True,
+                            widget=forms.PasswordInput(attrs={'class': 'input-mini', 'required': 'required'}))
     captcha = forms.IntegerField(label=u'验证码', required=True,
                                  widget=forms.TextInput(attrs={'class': 'input-mini', 'required': 'required'}))
 
@@ -13,5 +15,5 @@ class CheckForm(forms.Form):
 class UploadDbfForm(forms.Form):
     tests = NCRE.objects.all()
     test_list = [(t.pk, t.testDate) for t in tests]
-    import_to = forms.CharField(label=u'导入位置', widget=forms.Select(choices=test_list, attrs={'required': 'required'}))
+    ncre = forms.CharField(label=u'导入位置', widget=forms.Select(choices=test_list, attrs={'required': 'required'}))
     dbf_file = forms.FileField(label=u'DBF文件', widget=forms.FileInput(attrs={'required': 'required'}))
