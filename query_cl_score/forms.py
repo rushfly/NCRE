dd682ad3948d2b723c6d15163529c5b688efee7b
weetao <weetao@yeah.net>
2013-12-04 20:43:09 +0800

    remov query_cl_sore app

--- query_cl_score/forms.py
+++
@@ -1,9 +0,0 @@
-#coding=utf8
-from django import forms
-
-
-class CheckForm(forms.Form):
-    testId = forms.CharField(max_length=16, label=u'准考证号', required=True,
-                             widget=forms.TextInput(attrs={'class': 'input-medium', 'required': 'required'}))
-    captcha = forms.IntegerField(label=u'验证码', required=True,
-                                 widget=forms.TextInput(attrs={'class': 'input-mini', 'required': 'required'}))
--- query_cl_score/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from query_cl_score import views
-
-urlpatterns = patterns(
-    '',
-    url(r'^check/$', views.check),
-    url(r'^result/$',views.show_result),
-    url(r'^import/$',views.import_dbf),
-)
