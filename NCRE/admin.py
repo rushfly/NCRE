e2fb18d6b30b938f306ffad00c64597e66c1e369
weetao <weetao@yeah.net>
2013-12-07 01:10:49 +0800

    add TestScore Admin

--- NCRE/admin.py
+++ NCRE/admin.py
@@ -1,11 +1,17 @@
 #coding=utf8
 from django.contrib import admin
-from NCRE.models import NCRE
+from NCRE.models import NCRE, TestScore
 
 
 class NCREAdmin(admin.ModelAdmin):
-    list_display = ('id',  'testDate', 'place')
+    list_display = ('id', 'testDate', 'place')
     ordering = ('id',)
 
 
+class TestScoreAdmin(admin.ModelAdmin):
+    list_display = ('testId', 'stuName', 'certId')
+    list_filter = ('ncre',)
+
+
 admin.site.register(NCRE, NCREAdmin)
+admin.site.register(TestScore,TestScoreAdmin)
